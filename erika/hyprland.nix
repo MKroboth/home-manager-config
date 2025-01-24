b66ec1c0df04e1a63d2a51156acfcf4f7c7cc80a
Maximilian Kroboth <maximilian@kroboth-home.at>
2025-01-24 15:30:30 +0100

    fix erika xwayland scaling

--- erika/hyprland.nix
+++ erika/hyprland.nix
@@ -3,12 +3,14 @@
   wayland.windowManager.hyprland.settings = {
 
     monitor = [
-      "eDP-1,1920x1080@60,0x0,1"
+      "eDP-1,highres,auto,1"
     ];
+    xwayland = {
+      force_zero_scaling = true;
+    };
     gestures = {
       # See https://wiki.hyprland.org/Configuring/Variables/ for more
       workspace_swipe = true;
     };
-
   };
 }
